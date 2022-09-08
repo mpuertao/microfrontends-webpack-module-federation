04d134c404a25513f2570802ded891bed63b1655
maupuert <maupuert@bancolombia.com.co>
2022-09-07 20:13:17 -0500

    feature: input with date payment onclick eventemitter

--- paymentdetails/src/PaymentDetails.js
+++ paymentdetails/src/PaymentDetails.js
@@ -3,7 +3,7 @@ import React from "react";
 const PaymentDetails = (props) => {
 
     const onPaymentChanged = () => {
-        props.emitter.emit("paymentChanged", "Abr 2021");
+        props.emitter.emit("paymentChanged", document.getElementById('paymentdate').value);
     }
 
     return (
@@ -11,8 +11,12 @@ const PaymentDetails = (props) => {
             <h3>Payment Details</h3>
             <ul>
                 <li><i>payment method: </i>credit card</li>
-                <li><a href="#" onClick={onPaymentChanged}>Change payment method to PayPal</a></li>
-                <li><a href="#">View payments history</a></li>
+                {/* <li><a href="#" onClick={onPaymentChanged}>Change payment method to PayPal</a></li> */}
+                <input type="text" id="paymentdate" name="paymentdate"/>
+                {/* <input type="button" value="Change Payment Date" onClick={onPaymentChanged}/> */}
+                <button onClick={onPaymentChanged}>Ingress Payment Date</button>
+                <li><a href="#">View payments history</a>
+                </li>
             </ul>
         </div>
     );
